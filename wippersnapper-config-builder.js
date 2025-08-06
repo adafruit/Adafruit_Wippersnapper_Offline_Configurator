684427ae772796b2f312f7e789450539cb74f886
Tyeth Gundry <tyethgundry@googlemail.com>
2025-08-06 17:45:55 +0100

    Add Expansion Board Base for XIAO

--- wippersnapper-config-builder.js
+++ wippersnapper-config-builder.js
@@ -126,6 +126,14 @@ const companionBoardConfigs = {
         productURL: 'https://www.adafruit.com/product/4285',
         documentationURL: 'https://learn.adafruit.com/adafruit-airlift-shield-esp32-wifi-co-processor',
         image: 'https://cdn-shop.adafruit.com/640x480/4285-05.jpg'
+    },
+    'seeed-xiao-ssd1306-expansion-base': {
+        rtc: 'PCF8563',
+        sdCardCS: 'D2',
+        extras: 'Display',
+        productURL: 'https://www.seeedstudio.com/Seeeduino-XIAO-Expansion-board-p-4746.html',
+        documentationURL: 'https://wiki.seeedstudio.com/Seeeduino-XIAO-Expansion-Board/',
+        image: 'https://files.seeedstudio.com/wiki/Seeeduino-XIAO-Expansion-Board/Update_pic/zheng1.jpg'
     }
 };
 
