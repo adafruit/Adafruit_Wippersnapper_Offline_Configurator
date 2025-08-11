c1654bcc1f4f620ad3f441fe3234f008ea6cb9a9
Tyeth Gundry <tyethgundry@googlemail.com>
2025-08-11 12:55:12 +0100

    offline-mode branch for board/component urls

--- load-wippersnapper-data.js
+++ load-wippersnapper-data.js
@@ -1,8 +1,8 @@
 // Load Wippersnapper boards and components data
 
 // Configuration - technically unused (instead ./ relative links) but useful for reference
-const BOARDS_JSON_URL = 'https://raw.githubusercontent.com/adafruit/Adafruit_Wippersnapper_Offline_Configurator/refs/heads/main/wippersnapper_boards.json'; //'wippersnapper_boards.json';
-const COMPONENTS_JSON_URL = 'https://raw.githubusercontent.com/adafruit/Adafruit_Wippersnapper_Offline_Configurator/refs/heads/main/wippersnapper_components.json'; //'wippersnapper_components.json';
+const BOARDS_JSON_URL = 'https://raw.githubusercontent.com/adafruit/Adafruit_Wippersnapper_Offline_Configurator/refs/heads/offline-mode/wippersnapper_boards.json'; //'wippersnapper_boards.json';
+const COMPONENTS_JSON_URL = 'https://raw.githubusercontent.com/adafruit/Adafruit_Wippersnapper_Offline_Configurator/refs/heads/offline-mode/wippersnapper_components.json'; //'wippersnapper_components.json';
 
 // Global app state
 const appState = {
